35bf6bf2d52f7be87ad6d01d219f918f205ffa61
Piotr Witkos <piotrek@Piotrs-MacBook-Pro.local>
2020-10-30 12:49:02 +0100

    initial

--- task.py
+++ task.py
@@ -14,17 +14,51 @@
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface (might be text-like).
 #
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
+# Try to expand your implementation as best as you can.
+# Think of as many features as you can, and try implementing them.
+# Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
+# Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
+# The goal of this task is for you to SHOW YOUR BEST python programming skills.
+# Impress everyone with your skills, show off with your code.
 #
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
+# Your program must be runnable with command "python task.py".
+# Show some usecases of your library in the code (print some things)
 #
-#When you are done upload this code to your github repository. 
+# When you are done upload this code to your github repository.
 #
-#Delete these comments before commit!
-#Good luck.
+# Delete these comments before commit!
+# Good luck.
+
+class Student:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.frequency = 100
+        self.marks = []
+
+    def get_average_score(self):
+        scoreSum = 0
+        for mark in self.marks:
+            scoreSum += mark
+        return scoreSum/len(self.marks)
+
+
+class Highscool:
+    def __init__(self, *studClasses):
+        self.studClasses = studClasses
+
+    def print_avarage_score_in_classes(self):
+
+        countClass = 0
+        for studClass in self.studClasses:
+            scoreSum = 0
+            countClass += 1
+            for student in studClass:
+                scoreSum += student.get_average_score()
+            scoreAverage = scoreSum/len(studClass)
+            print(
+                f"The average score of class nr. {countClass} is {scoreAverage}")
+
+
+if __name__ == "__main__":
+    someSchool = Highscool()
