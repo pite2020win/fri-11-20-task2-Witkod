55554f1870e66055d05c84343e773f83825ddcca
Piotr Witkos <piotrek@Piotrs-MacBook-Pro.local>
2020-11-12 23:18:24 +0100

    improved code

--- task.py
+++ task.py
@@ -1,65 +1,88 @@
-# Class diary
-#
-# Create program for handling lesson scores.
-# Use python to handle student (highscool) class scores, and attendance.
-# Make it possible to:
-# - Get students total average score (average across classes)
-# - get students average score in class
-# - hold students name and surname
-# - Count total attendance of student
-#
-# Please, use your imagination and create more functionalities.
-# Your project should be able to handle entire school(s?).
-# If you have enough courage and time, try storing (reading/writing)
-# data in text files (YAML, JSON).
-# If you have even more courage, try implementing user interface (might be text-like).
-#
-# Try to expand your implementation as best as you can.
-# Think of as many features as you can, and try implementing them.
-# Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-# Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-# The goal of this task is for you to SHOW YOUR BEST python programming skills.
-# Impress everyone with your skills, show off with your code.
-#
-# Your program must be runnable with command "python task.py".
-# Show some usecases of your library in the code (print some things)
-#
-# When you are done upload this code to your github repository.
-#
-# Delete these comments before commit!
-# Good luck.
-
-class Student:
-    def __init__(self, name, surname, *marks):
-        self.name = name
-        self.surname = surname
-        self.frequency = 100
-        self.marks = marks
-
-    def get_average_score(self):
-        scoreSum = 0
-        for mark in self.marks:
-            scoreSum += mark
-        return scoreSum/len(self.marks)
+import json
+import statistics
+import logging
+import functools
 
 
-class Highscool:
-    def __init__(self, *studClasses):
-        self.studClasses = studClasses
+def CheckArgumentsWrapper(object):
+    def create_function_with_argument_check(function):
+        def function_with_argument_check(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except KeyError:
+                return f"The argument should be one of the existing {object}"
+        return function_with_argument_check
+    return create_function_with_argument_check
 
-    def print_avarage_score_in_classes(self):
 
-        countClass = 0
-        for studClass in self.studClasses:
-            scoreSum = 0
-            countClass += 1
-            for student in studClass:
-                scoreSum += student.get_average_score()
-            scoreAverage = scoreSum/len(studClass)
+@CheckArgumentsWrapper("Student")
+def get_student_average_score(student):
+    return statistics.mean(student['marks'])
+
+
+@CheckArgumentsWrapper("Student")
+def check_attendance(student):
+    present = list(
+        filter(lambda is_present: is_present, student['attendance']))
+    return len(present)/len(student['attendance'])
+
+
+@CheckArgumentsWrapper('highschool class')
+def get_class_average_score(highschool_class):
+    students_average_score_list = []
+    for student in highschool_class['students']:
+        students_average_score_list.append(get_student_average_score(student))
+    return statistics.mean(students_average_score_list)
+
+
+@CheckArgumentsWrapper('highschool class')
+def get_class_average_attendance(highschool_class):
+    students_average_attendance = []
+    for student in highschool_class['students']:
+        students_average_attendance.append(check_attendance(student))
+    return statistics.mean(students_average_attendance)
+
+
+@CheckArgumentsWrapper('highschool')
+def get_highschool_average_score(highschool):
+    class_average_score_list = []
+    for highschool_class in highschool['classes']:
+        class_average_score_list.append(
+            get_class_average_score(highschool_class))
+    return statistics.mean(class_average_score_list)
+
+
+@CheckArgumentsWrapper('highschool')
+def get_highschool_average_attendance(highschool):
+    class_average_attendance = []
+    for highschool_class in highschool['classes']:
+        class_average_attendance.append(
+            get_class_average_attendance(highschool_class))
+    return statistics.mean(class_average_attendance)
+
+
+def load_highscool_data(json_file_name):
+    with open(json_file_name, 'r') as json_file:
+        json_object = json.load(json_file)
+    return json_object
+
+
+@CheckArgumentsWrapper("schools")
+def print_general_info_about_school(schools):
+    for school in schools:
+        print(f"""School name: {school['name']}
+            School average score: {get_highschool_average_score(school)}
+            School average attendance: {get_highschool_average_attendance(school)}""")
+        for school_class in school['classes']:
             print(
-                f"The average score of class nr. {countClass} is {scoreAverage}"
+                f"""    Class {school_class['level']} 
+                Class average score: {get_class_average_score(school_class)},
+                Class average attendance: {get_class_average_attendance(school_class)}""")
 
 
 if __name__ == "__main__":
-    someSchool=Highscool([[Student("Adam", "Nowak", [4, 5, 3])]])
-    someSchool.print_avarage_score_in_classes()
+    logging.basicConfig(level=logging.INFO)
+
+    schools = load_highscool_data("data.json")
+
+    print_general_info_about_school(schools)
